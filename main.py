78e9ee0e63cc328ee6228960db7cfe3fd3212284
japaabruno <machado.bruno94@gmail.com>
2022-11-03 19:04:28 -0300

    Magre
    bot magre

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from time import sleep
+from turtle import pos
 import button
 import keyboard
 import pyautogui
+import json
 
-right = (1035,508,50,50)
-left = (850,508,50,50)
-top = (944,415,50,50)
-bot = (943,600,50,50)
+right = (1019,486,70,70)
+left = (835,486,70,70)
+top = (926,394,70,70)
+bot = (926,579,70,70)
 
 list_positions = [right, left, top, bot]
 
@@ -17,7 +19,7 @@ def move(location):
 def get_bush(location):
   if location != None:
     move(location)
-    keyboard.press(button.key['BACKSPACE'], 0.5)
+    keyboard.press(button.key['F2'], 0.5)
     pyautogui.click()
     sleep(4)
 
@@ -25,17 +27,52 @@ def move_and_click(location):
   move(location)
   pyautogui.click()
 
-for index in range(7):
-  while True:
-    position_in_map = pyautogui.locateOnScreen('./flags/flag_{}.png'.format(index), confidence=0.80)
-    print(position_in_map)
-    if position_in_map != None:
-      move_and_click(position_in_map)
-      sleep(6.5)
-      check_position = pyautogui.locateOnScreen('./flags/flag_{}.png'.format(index), confidence=0.80)
-      if check_position == None:
-        for position in list_positions:
-          for index in range(3):
-            bush = pyautogui.locateOnScreen('bushs/bush_{}.PNG'.format(index), confidence=0.85, region=position)
-            get_bush(bush)
+# for index in range(7):
+#   while True:
+#     position_in_map = pyautogui.locateOnScreen('./flags/flag_{}.png'.format(index), confidence=0.80)
+#     print("position_in_map", position_in_map)
+#     if position_in_map != None:
+#       move_and_click(position_in_map)
+#       sleep(6.5)
+#       check_position = pyautogui.locateOnScreen('./flags/flag_{}.png'.format(index), confidence=0.80)
+#       if check_position == None:
+#         for position in list_positions:
+#           for index in range(3):  
+#             print('tentando localizar bush')    
+#             bush = pyautogui.locateOnScreen('bushs/bush_{}.PNG'.format(index), confidence=0.85, region=position)
+#             print("bush", bush)
+#             get_bush(bush)
+#         break
+
+with open('infos.json', 'r') as file:
+    info = file.read()
+load_infos = json.loads(info)
+
+cont = 0
+while True:
+  for item in load_infos:
+    while True:
+      position_in_map = pyautogui.locateOnScreen(item['path'], confidence=0.82)
+      print(position_in_map, item)
+      if cont == 6:
+        cont=0
         break
+      cont= cont+1 
+      if position_in_map != None:
+        cont=0
+        move_and_click(position_in_map)
+        sleep(item["wait"])
+        check_position = pyautogui.locateOnScreen(item['path'], confidence=0.80)
+        if check_position == None:
+          for position in list_positions:
+            for item in range(3):
+              detected_bush = pyautogui.locateOnScreen('bushs/bush_{0}.PNG'.format(item), confidence=0.85, region=position)
+              get_bush(detected_bush)
+          break
+
+# while True:
+#   for position in list_positions:
+#     for item in range(3):
+#       detected_bush = pyautogui.locateOnScreen('bushs/bush_{}.PNG'.format(0), confidence=0.85, region=position)
+#       print(detected_bush)
+#       get_bush(detected_bush)
--- screenshot.py
+++ screenshot.py
@@ -1,25 +1,53 @@
-import pyautogui
+from importlib.resources import path
+import json
 from pynput.mouse import Listener
-
+import os
+import pyautogui
+import datetime;
 
 class Photo:
   def __init__(self):
-    self.count = 0
+    self.count = 100
+    self.coordiantes = []
+    self.timestemp = 0
+    self.time = 0
 
-  def take_photo(self, x, y):
-    my_screen = pyautogui.screenshot(region=(x - 10, y -10, 20, 20))
-    path = 'flags/flag_{}.png'.format(self.count)
-    self.count = self.count + 1
-    my_screen.save(path)
+  def take_photo(self, mouseX, mouseY):
+      format_x = mouseX - 10
+      format_y = mouseY - 10
+      myScreenshot = pyautogui.screenshot(region=(format_x, format_y, 20, 20))
+      # path = os.path.join(os.path.dirname(__file__), "my_flags", "flag_{0}.png".format(self.count))
+      path="flags/flag_{}.PNG".format(self.count)
+      self.count = self.count + 1
+      myScreenshot.save(path)
+      now = datetime.datetime.now().timestamp()
+      if self.timestemp == 0:
+        self.timestemp = now
+        infos = {
+        "path": path,
+        "wait": 3
+        }
+        self.coordiantes.append(infos)
+      else:
+        self.time = self.timestemp - now
+        infos = {
+        "path": path,
+        "wait": abs(int(self.time)) + 3
+        }
+        self.timestemp = now
+        self.coordiantes.append(infos)
+      return
 
   def click(self, x, y, button, pressed):
-    if button.name == 'right':
-      return False
-    if pressed:
-      self.take_photo(x, y)
-
+      print(button.name)
+      if button.name == 'right':
+        with open('infos.json', 'w') as file:
+         file.write(json.dumps(self.coordiantes))
+        return False
+      if pressed:
+        self.take_photo(x, y)
 
 photo = Photo()
 
 with Listener(on_click=photo.click) as listener:
-  listener.join()
+    listener.join()
